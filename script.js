7f056e44bf9e9f0b13e252c4d45616c0fe3db25c
Abhishek97Ojha <abhishek97ojha@gmail.com>
2023-01-10 00:18:56 +0530

    AdvanceJs-403-02.

---
+++ script.js
@@ -0,0 +1,63 @@
+let w = document.getElementById("w");
+w.addEventListener("click", () => {
+    w.setAttribute("style", "opacity:0.3");
+    let w1 = new Audio("sounds/w.mp3");
+    w1.play();
+    });
+w.addEventListener("mouseleave",()=>{
+    w.removeAttribute("style", "opacity:0.3");
+});
+let a = document.getElementById("a");
+a.addEventListener("click", () => {
+    a.setAttribute("style", "opacity:0.3");
+    let a1 = new Audio("sounds/a.mp3");
+    a1.play();
+});
+a.addEventListener("mouseleave",()=>{
+    a.removeAttribute("style", "opacity:0.3");
+});
+let s = document.getElementById("s");
+s.addEventListener("click", () => {
+    s.setAttribute("style", "opacity:0.3");
+    let s1 = new Audio("sounds/s.mp3");
+    s1.play();
+});
+s.addEventListener("mouseleave",()=>{
+    s.removeAttribute("style", "opacity:0.3");
+});
+let d = document.getElementById("d");
+d.addEventListener("click", () => {
+    d.setAttribute("style", "opacity:0.3");
+    let d1 = new Audio("sounds/d.mp3");
+    d1.play();
+});
+d.addEventListener("mouseleave",()=>{
+    d.removeAttribute("style", "opacity:0.3");
+});
+let j = document.getElementById("j");
+j.addEventListener("click", () => {
+    j.setAttribute("style", "opacity:0.3");
+    let j1 = new Audio("sounds/j.mp3");
+    j1.play();
+});
+j.addEventListener("mouseleave",()=>{
+    j.removeAttribute("style", "opacity:0.3");
+});
+let k = document.getElementById("k");
+k.addEventListener("click", () => {
+    k.setAttribute("style", "opacity:0.3");
+    let k1 = new Audio("sounds/k.mp3");
+    k1.play();
+});
+k.addEventListener("mouseleave",()=>{
+    k.removeAttribute("style", "opacity:0.3");
+});
+let l = document.getElementById("l");
+l.addEventListener("click", () => {
+    l.setAttribute("style", "opacity:0.3");
+    let l1 = new Audio("sounds/l.mp3");
+    l1.play();
+});
+l.addEventListener("mouseleave",()=>{
+    l.removeAttribute("style", "opacity:0.3");
+});
